c883ae719b94dcd11d382ff2a4c1e0bf2f9d4f7c
kennethormandy <hello@kennethormandy.com>
2020-11-07 15:29:53 -0800

    Adds testing dev dependencies and files

---
+++ tests/_craft/config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'password' => getenv('DB_PASSWORD'),
+    'user' => getenv('DB_USER'),
+    // 'database' => getenv('DB_DATABASE'),
+    'database' => 'testdb', 
+    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
+    'driver' => getenv('DB_DRIVER'),
+    'port' => getenv('DB_PORT'),
+    'schema' => getenv('DB_SCHEMA'),
+    'server' => getenv('DB_SERVER'),
+];
