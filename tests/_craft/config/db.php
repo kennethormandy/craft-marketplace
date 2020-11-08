f985366d681d9d81956896b63ba935f392d30962
kennethormandy <hello@kennethormandy.com>
2020-11-07 17:20:25 -0800

    Improves .env file setup to allow for separate db with test-specific environment variables

--- tests/_craft/config/db.php
+++ tests/_craft/config/db.php
@@ -1,13 +1,12 @@
 <?php
 
 return [
-    'password' => getenv('DB_PASSWORD'),
-    'user' => getenv('DB_USER'),
-    // 'database' => getenv('DB_DATABASE'),
-    'database' => 'testdb', 
-    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
-    'driver' => getenv('DB_DRIVER'),
-    'port' => getenv('DB_PORT'),
-    'schema' => getenv('DB_SCHEMA'),
-    'server' => getenv('DB_SERVER'),
+    'password' => getenv('DB_TEST_PASSWORD') ?? getenv('DB_PASSWORD'),
+    'user' => getenv('DB_TEST_USER') ?? getenv('DB_USER'),
+    'database' => getenv('DB_TEST_DATABASE') ?? getenv('DB_DATABASE'),
+    'tablePrefix' => getenv('DB_TEST_TABLE_PREFIX') ?? getenv('DB_TABLE_PREFIX'),
+    'driver' => getenv('DB_TEST_DRIVER') ?? getenv('DB_DRIVER'),
+    'port' => getenv('DB_TEST_PORT') ?? getenv('DB_PORT'),
+    'schema' => getenv('DB_TEST_SCHEMA') ?? getenv('DB_SCHEMA'),
+    'server' => getenv('DB_TEST_SERVER') ?? getenv('DB_SERVER'),
 ];
