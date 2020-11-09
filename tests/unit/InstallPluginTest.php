db7fece1d652642c189b0c644acaf428aa5ce55d
kennethormandy <hello@kennethormandy.com>
2020-11-09 10:56:58 -0800

    Adds basic test to check setup and plugin installed

---
+++ tests/unit/InstallPluginTest.php
@@ -0,0 +1,48 @@
+<?php 
+namespace kennethormandy\marketplace\tests;
+
+use Codeception\Test\Unit;
+use UnitTester;
+use Craft;
+
+class InstallPluginTest extends Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+    
+    public function testChangeEditionExample()
+    {
+        Craft::$app->setEdition(Craft::Pro);
+
+        codecept_debug('Craft set edition example test');
+
+        $this->assertSame(
+            Craft::Pro,
+            Craft::$app->getEdition());
+    }
+
+    public function testCraftDatabaseIsActive()
+    {
+        $dbIsActive = Craft::$app->getDb()->getIsActive();
+        $this->assertTrue($dbIsActive);
+    }
+
+    public function testMarketplacePluginIsInstalled()
+    {
+        $pluginMarketplace = Craft::$app->plugins->getPlugin('marketplace');
+        $pluginFake = Craft::$app->plugins->getPlugin('asdf');
+
+        $this->assertNotNull($pluginMarketplace);
+        $this->assertNull($pluginFake);
+    }
+}
