7e7b202e24be1669d0924816990f6f690a8e146b
Kenneth Ormandy <hello@kennethormandy.com>
2021-01-16 14:11:43 -0800

    Adds basic acceptance test that works with PHP browser

---
+++ tests/acceptance/ViewBuyCest.php
@@ -0,0 +1,40 @@
+<?php 
+
+class ViewBuyCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function signInSuccessfully(AcceptanceTester $I)
+    {
+        $I->amOnPage('/buy');
+        $I->see('Buy your hand-crafted');
+        $I->click('Buy The Last Knee-High ($60.00)', '.m-10 a');
+        $I->see('Adding');
+
+        // The form is auto-submitted via JS, which we don’t have
+        // in the PHP browser, so we submit the form ourselves
+        $I->click('.add-to-cart-form input[type="submit"]');
+
+        // /buy?step=2
+        $I->see('Your Address');
+        $I->fillField('#firstName', 'Jane');
+        $I->fillField('#addressLastName', 'Example');
+        $I->fillField('#address1', 'Example');
+        $I->fillField('#address2', 'Example');
+        $I->fillField('#city', 'Example');
+        $I->fillField('#zipCode', 'Example');
+        $I->fillField('#stateValue', 'EX');
+        $I->click('Next');
+
+        // /buy?step=1
+        $I->see('Your Email');
+        $I->fillField('#email', 'hi@example.com');
+        $I->click('Next');
+
+        // /buy?step=2
+        $I->see('Your Address');
+        $I->click('Next');
+    }
+}
