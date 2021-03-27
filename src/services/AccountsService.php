ed1fc5ac017dbac0e935a33ae1a2e02ee98e80ac
Kenneth Ormandy <hello@kennethormandy.com>
2021-03-27 12:21:30 -0700

    Adds support for redirect param, adds tests

--- src/services/AccountsService.php
+++ src/services/AccountsService.php
@@ -4,6 +4,7 @@ namespace kennethormandy\marketplace\services;
 
 use Craft;
 use craft\base\Component;
+use craft\helpers\UrlHelper;
 use kennethormandy\marketplace\Marketplace;
 use Stripe\Account as StripeAccount;
 use Stripe\Stripe;
@@ -23,6 +24,15 @@ class AccountsService extends Component
 
         $link = null;
 
+        // NOTE Stripe specific
+        $stripeParams = [];
+        if (isset($params->redirect)) {
+            $redirectUrl = $this->_getStripeRedirectUrl($params->redirect);
+            if ($redirectUrl) {
+                $stripeParams['redirect_url'] = $redirectUrl;
+            }
+        }
+
         // $plugin->getSettings didn’t allow me to test?
         $secretApiKey = Marketplace::$plugin->settings->getSecretApiKey();
 
@@ -39,7 +49,7 @@ class AccountsService extends Component
         }
 
         try {
-            $resp = StripeAccount::createLoginLink($accountId, $params);
+            $resp = StripeAccount::createLoginLink($accountId, $stripeParams);
             if ($resp->url) {
                 $link = $resp->url;
             }
@@ -49,5 +59,64 @@ class AccountsService extends Component
         }
 
         return $link;
+    }
+
+    private function _getStripeRedirectUrl($redirectUrlWithHash = null, $fallback = null)
+    {
+        $logoutLink = null;
+
+        if ($redirectUrlWithHash === null) {
+            // redirectInput wasn’t set
+            // $redirectUrl = $request->referrer;
+            $redirectUrl = $fallback;
+        } else {
+            // redirectInput was set, but but also be blank or invalid
+            $redirectUrl = $this->_getRedirectInputUrl($redirectUrlWithHash);
+        }
+
+        if ($redirectUrl === '') {
+            // redirectInput was blank, restore default Stripe behaviour with no params
+            return $logoutLink;
+        }
+
+        if ($redirectUrl === null) {
+            // Didn’t get a valid URL from _getRedirectInputUrl, so it might have been modified
+            // Replace it with the referrer as a fallback
+            // $redirectUrl = $request->referrer;
+            $redirectUrl = $fallback;
+
+            // The redirectInput value might have been modified, we couldn’t validate it
+            $redirectUrlWithHash = null;
+        }
+
+        if ($redirectUrlWithHash === null && isset($redirectUrl)) {
+            // Hash the referrer so we can validate it when we redirect
+            // back from the Dashboard. The $redirectUrlWithHash result would have
+            // already been hashed, but the referrer isn’t yet.
+            $redirectUrlWithHash = Craft::$app->getSecurity()->hashData(UrlHelper::url($redirectUrl));
+        }
+
+        if (isset($redirectUrl)) {
+            $logoutLink = UrlHelper::actionUrl('marketplace/accounts/create-logout-link', [
+                'redirect' => $redirectUrlWithHash,
+            ]);
+            $logoutLink = UrlHelper::url($logoutLink);
+        }
+
+        return $logoutLink;
+    }
+
+    private function _getRedirectInputUrl($redirectInputUrl)
+    {
+        $validUrl = Craft::$app->security->validateData($redirectInputUrl);
+
+        if ($validUrl === false) {
+            // If the URL from the redirectInput isn’t valid,
+            // this is the same result as if the input hadn’t been set,
+            // ie. will return the referrer.
+            return null;
+        }
+
+        return $validUrl;
     }
 }
--- tests/codeception/unit/AccountsServiceTest.php
+++ tests/codeception/unit/AccountsServiceTest.php
@@ -4,9 +4,9 @@ namespace kennethormandy\marketplace\tests;
 
 use Codeception\Test\Unit;
 use Craft;
-use UnitTester;
-use kennethormandy\marketplace\services\AccountsService;
+use craft\helpers\UrlHelper;
 use kennethormandy\marketplace\Marketplace;
+use UnitTester;
 
 class AccountsServiceTest extends Unit
 {
@@ -25,7 +25,7 @@ class AccountsServiceTest extends Unit
             $this->plugin,
             'settings',
             [
-                'getSecretApiKey' => getenv('STRIPE_SECRET_KEY')
+                'getSecretApiKey' => getenv('STRIPE_SECRET_KEY'),
             ],
             []
         );
@@ -45,9 +45,84 @@ class AccountsServiceTest extends Unit
         $this->assertStringStartsWith('https://connect.stripe.com/express/', $result);
     }
 
-    public function testCreateLoginLinkNoArg()
+    public function testCreateLoginLinkWithNoArgs()
     {
         $result = $this->plugin->accounts->createLoginLink();
         $this->assertEquals($result, null);
+    }
+
+    public function testCreateLoginLinkWithRedirect()
+    {
+        $accountId = getenv('STRIPE_CONNECT_USER_ACCOUNT_ID');
+        $result = $this->plugin->accounts->createLoginLink($accountId, [
+            'redirect' => 'https://example.com',
+        ]);
+
+        $this->assertNotEquals($result, null);
+        $this->assertIsString($result);
+        $this->assertStringStartsWith('https://connect.stripe.com/', $result);
+        $this->assertStringStartsWith('https://connect.stripe.com/express/', $result);
+    }
+
+    public function testRedirectUrlFromRedirectInputLikeFormat()
+    {
+        $exampleUrl = 'https://example.com';
+        $exampleUrlFromRedirectInput = Craft::$app->getSecurity()->hashData(UrlHelper::url($exampleUrl));
+
+        // We’re testing a private method here, which is ill-advised, but
+        // we can’t test the result of it via createLoginLink. The redirect
+        // is passed along to Stripe, and is only seen within the Stripe Connect
+        // Express UI itself.
+        $result = $this->invokeMethod($this->plugin->accounts, '_getStripeRedirectUrl', [$exampleUrlFromRedirectInput]);
+        $this->assertIsString($result);
+        $this->assertStringContainsString('actions', $result);
+        $this->assertStringEndsWith('https%3A//example.com', $result);
+    }
+
+    public function testRedirectUrlFromRedirectInputEmptyLikeFormat()
+    {
+        // The redirect can be explicitly be set to be blank, so the
+        // params sent to Stripe are empty, to restore the default
+        // behaviour (no redirect link).
+        $exampleUrl = '';
+        $exampleUrlFromRedirectInput = Craft::$app->getSecurity()->hashData($exampleUrl);
+        $result = $this->invokeMethod($this->plugin->accounts, '_getStripeRedirectUrl', [$exampleUrlFromRedirectInput]);
+        $this->assertIsNotString($result);
+        $this->assertEquals($result, null);
+    }
+
+    public function testRedirectUrlFromRedirectInputMissingLikeFormat()
+    {
+        // In practice, this is where the createLoginLink action would
+        // pass $request->referrer as a fallback URL if the redirect
+        // was not provided, or was missing
+        $fallbackUrl = 'https://kennethormandy.com';
+        $result = $this->invokeMethod($this->plugin->accounts, '_getStripeRedirectUrl', [
+            null,
+            $fallbackUrl,
+        ]);
+        $this->assertIsString($result);
+        $this->assertStringContainsString('actions', $result);
+        $this->assertStringEndsWith('https%3A//kennethormandy.com', $result);
+    }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     * @see https://stackoverflow.com/a/2798203/864799
+     * @see https://jtreminio.com/blog/unit-testing-tutorial-part-iii-testing-protected-private-methods-coverage-reports-and-crap/
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
     }
 }
