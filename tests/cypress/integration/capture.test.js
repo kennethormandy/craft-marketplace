d4a887fd6abfbd34b99a0633737e91fb6404b200
Kenneth Ormandy <hello@kennethormandy.com>
2021-04-30 10:09:58 -0700

    Updates capture test, formatting

--- tests/cypress/integration/capture.test.js
+++ tests/cypress/integration/capture.test.js
@@ -1,56 +1,87 @@
 context('Capture', () => {
-    it('Purchase using Manual Payment gateway', () => {
-      cy.visit(`${Cypress.env('CRAFT_DEFAULT_SITE_URL')}shop`)
+  it('Purchase using Manual Payment gateway', () => {
+    cy.visit(`${Cypress.env('CRAFT_DEFAULT_SITE_URL')}shop`)
 
-      cy.get('[name="purchasableId"][value="12"]').click();
-      cy.get('.fa-shopping-cart').click();
-      cy.get('.checkout-button').click();
+    const userToPay = {
+      name: 'Jane Example',
+      id: '15'
+    }  
 
-      cy.get('#email').type('hello+test@example.com');
-      cy.get('.button').click();
+    cy.get('[name="purchasableId"][value="12"]').click();
+    cy.get('.fa-shopping-cart').click();
+    cy.get('.checkout-button').click();
 
-      // Skip to payment step
-      cy.get('.steps > ul > :nth-child(5) > a').click();
+    cy.get('#email').type('hello+test@example.com');
+    cy.get('.button').click();
 
-      // Use the manual capture gateway
-      cy.get('#paymentMethod').select('gatewayId:3');
+    // Skip to payment step
+    cy.get('.steps > ul > :nth-child(5) > a').click();
 
-      cy.wait(5);
+    // Use the manual capture gateway
+    cy.get('#paymentMethod').select('gatewayId:3');
 
-      cy.get('.card-holder-first-name').type('Test');
-      cy.get('.card-holder-last-name').type('Testing');
+    // Wait for the change to take place, used instead of cy.wait
+    cy.contains('Updated the cart’s gatewayId')
 
-      cy.getWithinIframe('[name="cardnumber"]').type('4242424242424242');
-      cy.getWithinIframe('[name="exp-date"]').type('0130');
-      cy.getWithinIframe('[name="cvc"]', document).type('234')
+    cy.get('.card-holder-first-name').type('Test');
+    cy.get('.card-holder-last-name').type('Testing');
 
-      // The postal code may or may not be present, depending
-      // on how Stripe is configured.
-      cy.getWithinIframe('[name="postal"]').then(els => {
-          if (els.length) {
-              cy.getWithinIframe('[name="postal"]', document).type('12345')
-          }
-      });
+    cy.getWithinIframe('[name="cardnumber"]').type('4242424242424242');
+    cy.getWithinIframe('[name="exp-date"]').type('0130');
+    cy.getWithinIframe('[name="cvc"]', document).type('234')
 
-      cy.get('.button').click();
-      cy.wait(5);
+    // The postal code may or may not be present, depending
+    // on how Stripe is configured.
+    cy.getWithinIframe('[name="postal"]').then(els => {
+        if (els.length) {
+            cy.getWithinIframe('[name="postal"]', document).type('12345')
+        }
+    });
 
-      cy.get('#transactions-id').invoke('text').then((transactionId) => {
+    cy.get('.button').click();
 
-        // Manually capture the transactions,
-        // similar to what you’d do in the CMS dashboard
+    cy.contains('[data-test=payee]', userToPay.name);
+    cy.contains('[data-test=payee-id]', userToPay.id);
 
-        cy.visit(`${Cypress.env('CRAFT_DEFAULT_SITE_URL')}capture`)
-        cy.get('[name="loginName"]').type(Cypress.env('CRAFT_ADMIN_USERNAME'))
-  
-        cy.get('[name="password"]').type(Cypress.env('CRAFT_ADMIN_PASSWORD'))
-        cy.get('#login button').click()
-  
-        // Capture the relevant
-        cy.get(`#${transactionId} button`).click();
+    cy.get('[data-test=order-reference]:first-child').invoke('text').then((paymentIntentRef) => {
+      expect(paymentIntentRef).to.exist
+      cy.task('checkPaymentIntent', paymentIntentRef).then((result) => {
+        console.log(result)
 
-        cy.contains(`#${transactionId}`, `Transaction ${transactionId} Paid`);
+        // Check the info we have from Craft against the actual Stripe result
+        expect(result.amount).to.exist
+        expect(result.amount).to.equal(6000)
+
+        // The transaction won’t come through as already completed in this case
+        expect(result.status).to.equal('requires_capture')
+
+        expect(result.application_fee_amount).to.exist
+        expect(result.application_fee_amount).to.equal(600)
+        expect(result.transfer_data).to.exist
+        expect(result.transfer_data.destination).to.exist
+
+        cy.get('[data-test=payee-platform-id]').invoke('text').then((platformAccountId) => {
+          expect(result.transfer_data.destination).to.equal(platformAccountId)
+        })
       })
-
     })
+
+    cy.get('[data-test=transactions-id]').invoke('text').then((transactionId) => {
+
+      // Manually capture the transactions,
+      // similar to what you’d do in the CMS dashboard
+
+      cy.visit(`${Cypress.env('CRAFT_DEFAULT_SITE_URL')}capture`)
+      cy.get('[name="loginName"]').type(Cypress.env('CRAFT_ADMIN_USERNAME'))
+
+      cy.get('[name="password"]').type(Cypress.env('CRAFT_ADMIN_PASSWORD'))
+      cy.get('#login button').click()
+
+      // Capture the relevant
+      cy.get(`#${transactionId} button`).click();
+
+      cy.contains(`#${transactionId}`, `Transaction ${transactionId} Paid`);
+    })
+
+  })
 })
