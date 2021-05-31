4c2073491e0f04123df06d855011092686659a95
Kenneth Ormandy <hello@kennethormandy.com>
2021-05-30 18:07:42 -0700

    Adds sender to FeesEvent

--- src/events/FeesEvent.php
+++ src/events/FeesEvent.php
@@ -17,12 +17,18 @@ class FeesEvent extends ModelEvent
     // simply change the event entirely yourself. This would let you
     // set the value via line items, but you could also set it based on any
     // other details you might need to check.
+    // Changing: this all still applies, but the fees need to be calculated
+    // at the line item level instead of the order level. The event will
+    // still be Fees rather than Fee
 
     /** @var Fee[] */
     public $fees;
 
     /** @var Order */
     public $order;
+
+    /** @var LineItem */
+    public $sender;
 
     public $amount;
 }
