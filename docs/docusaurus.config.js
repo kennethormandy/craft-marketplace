229f13f544073ae515a30c1791fa8a0f4c4d5d43
Kenneth Ormandy <hello@kennethormandy.com>
2022-03-02 08:53:52 -0800

    Updates theme

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,5 +1,5 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
@@ -20,7 +20,7 @@ module.exports = {
         // "markup-templating", "twig",
         "php", "php-extras"
       ],
-      theme: lightCodeTheme,
+      theme: darkCodeTheme,
       darkTheme: darkCodeTheme,
     },
     image: 'images/og-image.png',
