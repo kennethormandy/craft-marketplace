10fee33fb16d303816662cba164425bc5581bd44
Kenneth Ormandy <hello@kennethormandy.com>
2024-08-27 16:28:35 -0700

    Moves account-checking from controller to service

--- src/controllers/AccountsController.php
+++ src/controllers/AccountsController.php
@@ -7,19 +7,9 @@ use craft\elements\User;
 use craft\web\Controller;
 use craft\web\Response;
 use kennethormandy\marketplace\Marketplace;
-use kennethormandy\marketplace\events\AccountAccessEvent;
-use Stripe\Exception\InvalidArgumentException;
-use Stripe\Exception\InvalidRequestException;
-use Stripe\Exception\PermissionException;
-use verbb\auth\helpers\Session;
 
 class AccountsController extends Controller
 {
-    // BEFORE_GATEWAY_ACCOUNT_ACCESS
-    // AFTER_GATEWAY_ACCOUNT_ACCESS
-    public const BEFORE_ACCOUNT_ACCESS = 'BEFORE_ACCOUNT_ACCESS';
-    public const AFTER_ACCOUNT_ACCESS = 'AFTER_ACCOUNT_ACCESS';
-
     public array|int|bool $allowAnonymous = ['create-logout-link'];
 
     public function actionCreateLoginLink(): ?Response
@@ -32,8 +22,6 @@ class AccountsController extends Controller
     public function actionCreateAccountLink(): ?Response
     {
         return $this->_createLink(function($accountId, $params) {
-
-            // TODO Pass params like redirect to Accounts service, and then onto Stripe?
             return Marketplace::getInstance()->accounts->createAccountLink($accountId, $params);
         });
     }
@@ -46,67 +34,8 @@ class AccountsController extends Controller
         // Note `accounts->createLoginLink` gets you the express dashboard and is what we’ve always used
         // Note `accountLinks->create` gets you an account link to refresh/finish your onboarding?
         $request = Craft::$app->getRequest();
-        $accountIdHandle = Marketplace::$plugin->handles->getButtonHandle();
 
-        $isValid = true;
-        $currentUser = Craft::$app->getUser();
-        $currentUserIdentity = $currentUser->getIdentity();
-        $elementType = User::class;
-        $element = $currentUserIdentity;
-
-        if ($request->getParam('elementUid')) {
-            $elementUid = $request->getParam('elementUid');
-            $elementType = Craft::$app->elements->getElementTypeByUid($elementUid) ?? $elementType;
-            $element = Craft::$app->elements->getElementByUid($elementUid, $elementType) ?? $element;
-        }
-
-        // Check if the User or other element has the field, and that the value matches.
-        if (!$element[$accountIdHandle]) {
-            $isValid = false;
-        }
-
-        $accountId = $element[$accountIdHandle];
-
-        // User must either be:
-        // - An admin
-        // - Requesting a link for their own user element
-        // - Requesting a link for another type of element, which they are related to
-        // - Approved by the custom logic from the AccountAccessEvent, which may override the prev. conditions
-        if (!$currentUser->getIsAdmin()) {
-            if ($elementType === User::class) {
-
-                // The custom element is a user, but it’s not the current user
-                if ($element->uid !== $currentUserIdentity->uid) {
-                    $isValid = false;
-                }
-
-            } else {
-
-                // The custom element is org-like, and by default should be related to the custom element
-                $orgLike = User::find()->id($currentUser->id)->relatedTo($element)->count();
-                if (!$orgLike) {
-                    $isValid = false;
-                }
-
-            }    
-        }
-
-        // Verify the end user can access this Stripe account—this allows developers to relax the default
-        // logic above (ex. have orgs that don’t relate to users), or implement their own conditions.
-        $event = new AccountAccessEvent();
-        $event->accountId = $accountId;
-        $event->sender = $element;
-        $event->isValid = $isValid;
-
-        if ($this->hasEventHandlers(self::BEFORE_ACCOUNT_ACCESS)) {
-            $this->trigger(self::BEFORE_ACCOUNT_ACCESS, $event);
-        }
-
-        if (!$event->isValid) {
-            Marketplace::$plugin->log('User ' . $currentUserIdentity . ' attempting to create link for account that isn’t their own, without admin access.', [], 'error');
-            $this->_setError('You do not have permission to access that account.');
-            return null;
-        }
+        $elementUid = $request->getParam('elementUid') ?? null;
 
         $params = (object) [
             'redirect' => null,
@@ -117,20 +46,9 @@ class AccountsController extends Controller
             $params->redirect = $request->getParam('redirect');
         }
 
-        try {
-            $resp = call_user_func($callback, $accountId, $params);
-        } catch (PermissionException|InvalidRequestException|InvalidArgumentException $e) {
-            // This error could occur if the account ID was wrong, revoked, didn’t match between test versus live, etc.
-            Marketplace::$plugin->log($e->getMessage(), [], 'error');
-            $event->isValid = false;
-        }
+        $resp = call_user_func($callback, $elementUid, $params);
 
-        if ($this->hasEventHandlers(self::AFTER_ACCOUNT_ACCESS)) {
-            $this->trigger(self::AFTER_ACCOUNT_ACCESS, $event);
-        }
-
-        if (!$event->isValid) {
-            $this->_setError('Unable to provide access to that account, or the account does not exist.');
+        if (!$resp || !$resp->url) {
             return null;
         }
 
@@ -144,23 +62,5 @@ class AccountsController extends Controller
         $validatedUrl = Craft::$app->security->validateData($redirectParam);
 
         return $this->redirect($validatedUrl);
-    }
-
-    /**
-     * Use Auth’s flash messages to provide Marketplace- or Stripe-specific errors.
-     */
-    private function _setError(string $message): void
-    {
-        // TODO Handle translations for $message
-        // TODO Decide whether to use Social Login errors, namespaced
-        // errors using Social Login’s session helper, or use `errorMessage`
-        // like before. Leaning towards option 2, which would require a
-        // different Twig helper than Social Login.
-
-        Session::setError('marketplace', $message);
-
-        Craft::$app->getUrlManager()->setRouteParams([
-            'variables' => ['errorMessage' => $message],
-        ]);
     }
 }
--- src/services/Accounts.php
+++ src/services/Accounts.php
@@ -3,54 +3,164 @@
 namespace kennethormandy\marketplace\services;
 
 use Craft;
+use craft\base\Element;
 use craft\base\Component;
+use craft\elements\User;
 use craft\helpers\UrlHelper;
 use kennethormandy\marketplace\Marketplace;
+use kennethormandy\marketplace\events\AccountAccessEvent;
+use Stripe\Exception\InvalidArgumentException;
+use Stripe\Exception\InvalidRequestException;
+use Stripe\Exception\PermissionException;
 use Stripe\StripeClient;
+use verbb\auth\helpers\Session;
 
 class Accounts extends Component
 {
+    public const BEFORE_ACCOUNT_ACCESS = 'BEFORE_ACCOUNT_ACCESS';
+    public const AFTER_ACCOUNT_ACCESS = 'AFTER_ACCOUNT_ACCESS';
+
     public function init(): void
     {
         parent::init();
     }
 
-    public function createLoginLink($accountId, $params = [
+    public function createLoginLink(Element|string|null $element = null, $params = [
         'redirect' => null,
         'referrer' => null,
     ])
     {
-        return $this->_getStripe()->accounts->createLoginLink($accountId);
+        return $this->_createLink($element, $params, function($accountId, $params) {
+            return $this->_getStripe()->accounts->createLoginLink($accountId, $params);
+        });
     }
 
     /**
      * @see https://docs.stripe.com/api/account_links/create
      */
-    public function createAccountLink($accountId = null, $params = [
+    public function createAccountLink(Element|string|null $element = null, $params = [
         'redirect' => null,
         'referrer' => null,
     ])
     {
-        // NOTE Stripe specific
-        $stripeParams = [
-            'account' => $accountId,
+        return $this->_createLink($element, $params, function($accountId, $params) {
 
-            // TODO These values are used, after you finish in the Stripe modal if you still have requirements
-            'refresh_url' => 'https://example.com/refresh',
-            'return_url' => 'https://example.com/return',
+            // NOTE Stripe specific
+            $stripeParams = [
+                'account' => $accountId,
 
-            'type' => 'account_onboarding',
-            'collection_options' => ['fields' => 'eventually_due'],
-        ];
+                // TODO These values are used, after you finish in the Stripe modal if you still have requirements
+                'refresh_url' => 'https://example.com/refresh',
+                'return_url' => 'https://example.com/return',
 
-        if (isset($params->redirect)) {
-            $redirectUrl = $this->_getRedirectUrl($params->redirect, $params->referrer);
-            if ($redirectUrl) {
-                $stripeParams['return_url'] = $redirectUrl;
+                'type' => 'account_onboarding',
+                'collection_options' => ['fields' => 'eventually_due'],
+            ];
+
+            if (isset($params->redirect)) {
+                $redirectUrl = $this->_getRedirectUrl($params->redirect, $params->referrer);
+                if ($redirectUrl) {
+                    $stripeParams['return_url'] = $redirectUrl;
+                }
             }
+
+            return $this->_getStripe()->accountLinks->create($stripeParams);
+        });
+    }
+
+    private function _createLink(Element|string|null $element, $params, $callback)
+    {
+        $isValid = true;
+        $accountIdHandle = Marketplace::$plugin->handles->getButtonHandle();
+
+        // Use a provided element, query an element via the provided UID, or fallback to the current user.
+        $currentUser = Craft::$app->getUser();
+        $currentUserIdentity = $currentUser->getIdentity();
+        $elementType = null;
+
+        if ($element) {
+            if (gettype($element) === 'string') {
+                // It’s an element UID only
+                $element = Craft::$app->elements->getElementByUid($element) ?? $currentUserIdentity;
+            }
+        } else {
+            $element = $currentUserIdentity;
         }
 
-        return $this->_getStripe()->accountLinks->create($stripeParams);
+        $elementType = $element->className();
+
+        // Check if the User or other element has the field, and that the value matches.
+        if (!$element || !$element[$accountIdHandle]) {
+            $isValid = false;
+        }
+
+        $accountId = $element[$accountIdHandle];
+
+        // User must either be:
+        // - An admin
+        // - Requesting a link for their own user element
+        // - Requesting a link for another type of element, which they are related to
+        // - Approved by the custom logic from the AccountAccessEvent, which may override the prev. conditions
+        // if (!$currentUser->getIsAdmin()) {
+            if ($elementType === User::class) {
+
+                // The custom element is a user, but it’s not the current user
+                if ($element->uid !== $currentUserIdentity->uid) {
+                    $isValid = false;
+                }
+
+            } else {
+
+                // The custom element is org-like, and by default should be related to the custom element
+                $orgLike = User::find()->id($currentUser->id)->relatedTo($element)->count();
+                if (!$orgLike) {
+                    $isValid = false;
+                }
+
+            }
+        // }
+
+        // Verify the end user can access this Stripe account—this allows developers to relax the default
+        // logic above (ex. have orgs that don’t relate to users), or implement their own conditions.
+        $event = new AccountAccessEvent();
+        $event->accountId = $accountId;
+        $event->sender = $element;
+        $event->isValid = $isValid;
+
+        if ($this->hasEventHandlers(self::BEFORE_ACCOUNT_ACCESS)) {
+            $this->trigger(self::BEFORE_ACCOUNT_ACCESS, $event);
+        }
+
+        if (!$event->isValid) {
+            Marketplace::$plugin->log('User ' . $currentUserIdentity . ' attempting to create link for account that isn’t their own, without admin access.', [], 'error');
+            $this->_setError('You do not have permission to access that account.');
+            return null;
+        }
+
+        Marketplace::$plugin->log('🙂');
+        Marketplace::$plugin->log($accountId);
+        
+        try {
+            $resp = call_user_func($callback, $accountId, $params);
+        } catch (PermissionException|InvalidRequestException|InvalidArgumentException $e) {
+            // This error could occur if the account ID was wrong, revoked, didn’t match between test versus live, etc.
+            Marketplace::$plugin->log($e->getMessage(), [], 'error');
+            $event->isValid = false;
+        }
+
+        if ($this->hasEventHandlers(self::AFTER_ACCOUNT_ACCESS)) {
+            $this->trigger(self::AFTER_ACCOUNT_ACCESS, $event);
+        }
+
+        if (!$event->isValid) {
+            $this->_setError('Unable to provide access to that account, or the account does not exist.');
+            return null;
+        }
+
+        Marketplace::$plugin->log('🙂');
+        Marketplace::$plugin->log($resp);
+
+        return $resp;
     }
 
     private function _getStripe(): StripeClient
@@ -118,5 +228,23 @@ class Accounts extends Component
         }
 
         return $validUrl;
+    }
+
+    /**
+     * Use Auth’s flash messages to provide Marketplace- or Stripe-specific errors.
+     */
+    private function _setError(string $message): void
+    {
+        // TODO Handle translations for $message
+        // TODO Decide whether to use Social Login errors, namespaced
+        // errors using Social Login’s session helper, or use `errorMessage`
+        // like before. Leaning towards option 2, which would require a
+        // different Twig helper than Social Login.
+
+        Session::setError('marketplace', $message);
+
+        Craft::$app->getUrlManager()->setRouteParams([
+            'variables' => ['errorMessage' => $message],
+        ]);
     }
 }
