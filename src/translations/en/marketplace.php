246f9bff65467ed715a396281fc7df3f3d3a6a9d
Kenneth Ormandy <hello@kennethormandy.com>
2024-08-30 14:50:41 -0700

    Adds translations

---
+++ src/translations/en/marketplace.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Connect' => 'Connect',
+    'Open Dashboard' => 'Open Dashboard',
+
+    // Errors
+    'You do not have permission to access that account.' => 'You do not have permission to access that account.',
+    'Unable to provide access to that account, or the account does not exist.' => 'Unable to provide access to that account, or the account does not exist.',
+];
