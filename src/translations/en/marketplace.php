bb27f26fd2bc3c926cf728e18b5acaaa1f659f58
Kenneth Ormandy <hello@kennethormandy.com>
2024-09-04 11:58:56 -0700

    Adds more translation strings

--- src/translations/en/marketplace.php
+++ src/translations/en/marketplace.php
@@ -1,10 +1,15 @@
 <?php
 
 return [
+    // Site & CP UI labels
     'Connect' => 'Connect',
     'Open Dashboard' => 'Open Dashboard',
 
-    // Errors
+    // CP UI labels
+    'Account ID' => 'Account ID',
+
+    // Site & CP error messages
+    'Account already exists.' => 'Account already exists.',
     'You do not have permission to access that account.' => 'You do not have permission to access that account.',
     'Unable to provide access to that account, or the account does not exist.' => 'Unable to provide access to that account, or the account does not exist.',
 ];
