3f284b73eb0186d06f502c1179335188403eec7c
Kenneth Ormandy <hello@kennethormandy.com>
2024-09-07 07:43:29 -0700

    Adds translation strings for settings

--- src/translations/en/marketplace.php
+++ src/translations/en/marketplace.php
@@ -8,6 +8,16 @@ return [
     // CP UI labels
     'Account ID' => 'Account ID',
 
+    'Redirect URI' => 'Redirect URI',
+    'Copy this URI into the Stripe Dashboard when setting up Stripe Connect.' => 'Copy this URI into the Stripe Dashboard when setting up Stripe Connect.',
+
+    'Stripe Connect Client ID' => 'Stripe Connect Client ID',
+    'Your Client ID for Stripe Connect, for Stripe accounts using OAuth. Note this is not the same as your Stripe publishable key.' => 'Your Client ID for Stripe Connect, for Stripe accounts using OAuth. Note this is not the same as your Stripe publishable key.',
+
+    'Stripe Secret Key' => 'Stripe Secret Key',
+    'The secret API key for the parent Stripe account, where other Stripe accounts will be connected. This is the same value you use in the Stripe for Craft Commerce gateway.' => 'The secret API key for the parent Stripe account, where other Stripe accounts will be connected. This is the same value you use in the Stripe for Craft Commerce gateway.',
+
+
     // Site & CP error messages
     'Account already exists.' => 'Account already exists.',
     'You do not have permission to access that account.' => 'You do not have permission to access that account.',
